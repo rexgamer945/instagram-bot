7add5c7fb2fcdb83178309db4d8052c5cb33b5da
rexgamer945 <75562022+rexgamer945@users.noreply.github.com>
2021-03-16 17:05:55 +1000

    Create instabot.py

---
+++ instabot.py
@@ -0,0 +1,6 @@
+bot = InstaBot('login', 'password',
+               like_per_day=1000,
+               more_than_likes=10,
+               tag_list = ['love', 'coding', 'dev'],
+               max_like_for_one_tag=5,
+               log_mod = 0)
